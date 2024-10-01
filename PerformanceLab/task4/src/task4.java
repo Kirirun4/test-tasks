d82ce0555e0c73af995c681576a741e50df646af
Kirill <88333835+Kirirun4@users.noreply.github.com>
2024-10-01 19:40:59 +0400

    Add files via upload

---
+++ PerformanceLab/task4/src/task4.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class task4 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File("src/numbers.txt"))) {
+            while (scanner.hasNext()) {
+                array.add(scanner.nextInt());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        int size = array.size();
+        if (size < 2)
+            throw new IllegalArgumentException("array's size < 2");
+
+        Collections.sort(array);
+        int middle = array.get(size / 2);
+        int sum = 0;
+        for (int a : array) {
+            sum += Math.abs(a - middle);
+        }
+        System.out.println(sum);
+    }
+}
